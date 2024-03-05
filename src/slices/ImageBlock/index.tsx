10ba91853087bdfb626f9e84303b696f85cf3520
DelDex <deljindavis159@gmail.com>
2024-03-05 22:17:56 +0100

    first commit

---
+++ src/slices/ImageBlock/index.tsx
@@ -0,0 +1,27 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `ImageBlock`.
+ */
+export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
+
+/**
+ * Component for "ImageBlock" Slices.
+ */
+const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicNextImage
+      field={slice.primary.image}
+      className="not-prose w-full h-full rounded-md  my-10 md:my-14 lg:my-16"
+    />
+    </section>
+  );
+};
+
+export default ImageBlock;
