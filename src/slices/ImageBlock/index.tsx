ed84c7b29a3db75338130388dc129bab08218c82
DelDex <deljindavis159@gmail.com>
2024-03-06 20:25:04 +0100

    fix 10

--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -17,6 +17,7 @@ const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       <PrismicNextImage
+      alt=""
       field={slice.primary.image}
       className="not-prose w-full h-full rounded-md  my-10 md:my-14 lg:my-16"
     />
